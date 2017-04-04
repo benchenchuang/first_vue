c615269d13041f2cac6408548a1338fa071e1100
benchenchuang <benchenchuang88@sina.com>
2017-04-04 21:28:47 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <h1 v-text='title'></h1>
+    <input v-model='newItem' v-on:keyup.enter='addNew'/>
+    <ul>
+      <li v-for='item in items' v-bind:class="{finished:item.isFinished}" v-on:click='toggleFinish(item)'>
+        {{item.label}}
+      </li>
+    </ul>
+    <h2 v-text='msg'></h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data:function(){
+    return {
+      title:'This is a todo list',
+      msg:'this is a msg',
+      items: [
+        {
+          label:'coding',
+          isFinished:false
+        },
+        {
+          label:'waking',
+          isFinished:true
+        },
+      ],
+      newItem:''
+    }
+  },
+  methods: {
+     toggleFinish:function(item){
+        item.isFinished = !item.isFinished
+      },
+      addNew:function(){
+        console.log(this.newItem);
+        this.items.push({
+          label:this.newItem,
+          isFinished:false
+        })
+        this.newItem = '';
+      }
+  }
+}
+</script>
+
+<style>
+.finished{
+  text-decoration:underline;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="hello" id='hello'>
+    <h1 v-text="title"></h1>
+    <input v-model='newItem'v-on:keyup.enter='addItem'/>
+    <ul>
+      <li v-for='item in items' v-bind:class={isFinished:item.isFinished} v-on:click='toggleFinish(item)'>
+      {{item.babel}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default{
+  data:function(){
+    return{
+      title:'this is add title',
+      items:[
+        {
+          babel:'babel one',
+          isFinished:false
+        },
+        {
+          babel:'babel two',
+          isFinished:true
+        }
+      ],
+      newItem:''
+    }
+  },
+  methods:{
+    toggleFinish:function(item){
+      item.isFinished=!item.isFinished;
+    },
+    addItem:function(){
+      this.items.push({
+        babel:this.newItem,
+        isFinished:false
+      })
+      this.newItem=''
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+.isFinished{
+  color:red;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
